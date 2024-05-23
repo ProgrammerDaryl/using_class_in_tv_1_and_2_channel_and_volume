ae4328dc0300b98dedda77a8c492bcf3412050b8
ProgrammerDaryl <ProgrammerDaryl>
2024-05-24 04:19:44 +0800

    Fixed Some Minor Issues

--- tv.py
+++ tv.py
@@ -1,11 +1,11 @@
 #make a blueprint
-class TV():
+class TV:
     #add data members (instance variables)
     def __init__(self, name):
         self.name = name
-        self.channel = channel = 1
-        self.volume = volume = 1
-        self.power = False
+        self.channel = 1
+        self.volume = 1
+        self.is_on = False
 
     #add instance method for power on
     def turnOn(self):
