bc9c16c88c10d711e3e47fe4abb2301852d4b3dd
ProgrammerDaryl <ProgrammerDaryl>
2024-05-24 04:00:35 +0800

    Added: Operations for TV1

--- tv_import.py
+++ tv_import.py
@@ -4,3 +4,10 @@ from tv import TV
 def main():
     tv_one = TV("tv1")
     tv_two = TV("tv2")
+
+    #operations for TV1
+    tv_one.turnOn()
+    tv_one.setChannel(30)
+    tv_one.setVolume(3)
+    tv_one.channelUp()
+
