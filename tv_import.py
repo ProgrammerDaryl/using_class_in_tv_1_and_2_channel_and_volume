5bcee917a6b0ecb7a990af831cb86727781a0d4c
ProgrammerDaryl <ProgrammerDaryl>
2024-05-24 04:20:21 +0800

    Fixed Some Minor Issues or Flaws

--- tv_import.py
+++ tv_import.py
@@ -2,23 +2,25 @@
 from tv import TV
 
 def main():
-    tv_one = TV("tv1")
-    tv_two = TV("tv2")
+    tv1 = TV("tv1")
+    tv2 = TV("tv2")
 
     #operations for TV1
-    tv_one.turnOn()
-    tv_one.setChannel(30)
-    tv_one.setVolume(3)
-    tv_one.channelUp()
+    tv1.turnOn()
+    tv1.setChannel(30)
+    tv1.setVolume(3)
+    tv1.channelUp()
 
     #operations for TV2
-    tv_two.turnOn()
-    tv_two.setChannel(3)
-    tv_two.setVolume(2)
-    tv_two.channel.Up()
+    tv2.turnOn()
+    tv2.setChannel(3)
+    tv2.setVolume(2)
+    tv2.channelUp()
 
     #output
     print("\n")
-    print(f"{tv_one.name}'s channel is {tv_one.getChannel()} and volume level is {tv_one.getVolume()}")
-    print(f"{tv_two.name}'s channel is {tv_two.getChannel()} and volume level is {tv_two.getVolume()}\n")
-    
+    print(f"{tv1.name}'s channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}")
+    print(f"{tv2.name}'s channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}\n")
+
+if __name__ == "__main__":
+    main()
