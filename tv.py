5cfb0cf4fc5f10af024a0dc0c8122347eba146aa
ProgrammerDaryl <ProgrammerDaryl>
2024-05-24 02:45:39 +0800

    Added: Instance Variables

--- tv.py
+++ tv.py
@@ -1,4 +1,9 @@
 #make a blueprint
-class tv():
-    def __init__(self) -> None:
+class TV():
+    #add data members (instance variables)
+    def __init__(self, channel=1, volume=1):
+        self.power = False
+        self.channel = channel
+        self.volume = volume
+    
         
