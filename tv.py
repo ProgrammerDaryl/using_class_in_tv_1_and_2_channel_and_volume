66270c45c76bfba5bf532732ba2475012d9d3897
ProgrammerDaryl <ProgrammerDaryl>
2024-05-24 03:18:33 +0800

    Added: for channels and volumes

--- tv.py
+++ tv.py
@@ -14,4 +14,22 @@ class TV():
     #add instance method for power off
     def turnOff(self):
         self.is_on = False
+
+    #add instance method for getting channel
+    def getChannel(self):
+        return self.channel
+    
+    #add instance method to set the channel
+    def setChannel(self, channel):
+        if 1 <= channel <= 120:
+            self.channel = channel
+    
+    #add instance method for getting volume
+    def getVolume(self):
+        return self.volume
+    
+    #add instance method to set the volume
+    def setVolume(self, volume):
+        if 1 <= volume <= 7:
+            self.volume = volume
     
